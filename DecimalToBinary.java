80b6e783ae4639ea7010c08004cf7b88b222d21a
AyeshaMalak <ayeshamalak34@gmail.com>
2026-04-28 20:48:37 +0500

    Create DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an Integer for conversion: ");
+        int number = input.nextInt();
+
+        String binary = "";
+
+        while(number > 0){
+            int remainder = number % 2;
+            binary = remainder + binary;
+            number = number / 2;
+        }
+
+        System.out.println("Binary: " + binary);
+    }
+}
